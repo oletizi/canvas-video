c5f67f9870dfaf368096db45dbb4480402553a48
orion <oletizi@mac.com>
2024-10-25 11:29:55 -0700

    * p5 rendering seems to work

---
+++ src/ts/app/client.ts
@@ -0,0 +1,22 @@
+import p5 from 'p5'
+
+
+
+let sketch = function(p) {
+    let x = 100;
+    let y = 100;
+
+    p.setup = function() {
+        p.createCanvas(window.innerWidth, window.innerHeight);
+    };
+
+    p.draw = function() {
+        p.background(0);
+        p.fill(255);
+        p.rect(x, y, 50, 50);
+    };
+};
+
+const p = new p5(sketch)
+
+
