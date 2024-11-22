1c0191892c241470da90fdd958b1575f624ac2ce
orion <oletizi@mac.com>
2024-11-21 21:54:26 -0800

    * transport and animation

--- src/ts/app/app.tsx
+++ src/ts/app/app.tsx
@@ -1,16 +1,20 @@
 import {createRoot} from "react-dom/client"
 import React from 'react'
-import {Container} from '@chakra-ui/react'
-import {Provider} from "../components/chakra/provider"
+import {Container, Group} from '@chakra-ui/react'
+import {Provider} from "@/components/chakra/provider"
 import p5 from "p5";
+import {Button} from "@/components/chakra/button";
+import {newTransport} from "@/app/transport";
 
-const e = document.getElementById('app')
+const r = document.getElementById('app')
 
+const transport = newTransport()
 let sketch = function (p) {
     let canvas
     p.preload = () => {
 
     }
+
     p.setup = () => {
         canvas = p.createCanvas(window.innerWidth, 500)
         canvas.parent('app-canvas')
@@ -19,21 +23,28 @@ let sketch = function (p) {
     p.draw = () => {
         p.background(127);
         p.fill(255);
-        p.rect(30, 20, 55, 55)
+        p.rect(30 + transport.getPosition(), 20, 55, 55)
+        transport.tick()
     }
 }
 const p = new p5(sketch)
 
-if (e) {
-    const root = createRoot(e)
+if (r) {
+    const root = createRoot(r)
     root.render(
         <Provider>
             <div id="app-canvas"></div>
             <Container>
                 <h1>Hello.</h1>
+                <Group attached>
+                    <Button onClick={() => transport.reset()}>Rew</Button>
+                    <Button onClick={() => transport.start()}>Start</Button>
+                    <Button onClick={() => transport.stop()}>Stop</Button>
+                </Group>
             </Container>
         </Provider>
     )
 } else {
     console.error(`Can't find id="app".`)
 }
+
---
+++ src/ts/app/transport.tsx
@@ -0,0 +1,51 @@
+export interface Transport {
+    start()
+
+    stop()
+
+    reset()
+
+    tick()
+
+    isRunning(): boolean
+
+    getPosition(): number
+}
+
+export function newTransport() {
+    return new BasicTransport()
+}
+
+class BasicTransport implements Transport {
+    private _isRunning: boolean = false
+    private _position: number = 0
+
+    constructor() {
+    }
+
+    isRunning(): boolean {
+        return this._isRunning;
+    }
+
+    getPosition(): number {
+        return this._position
+    }
+
+    reset() {
+        this._position = 0
+    }
+
+    start() {
+        this._isRunning = true
+    }
+
+    stop() {
+        this._isRunning = false
+    }
+
+    tick() {
+        if (this.isRunning()) {
+            this._position++
+        }
+    }
+}
