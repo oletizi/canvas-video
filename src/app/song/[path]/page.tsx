8751cb98d9e2dbd311a36239e92f503e669ba7b8
orion <oletizi@mac.com>
2024-12-28 18:20:51 -0800

    * fabricjs is sort of working

--- src/app/song/[path]/page.tsx
+++ src/app/song/[path]/page.tsx
@@ -1,23 +1,33 @@
 "use client"
 import {newSong} from "@/song/song";
 import {SongView} from "@/components/song-view";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useParams} from "next/navigation";
+import {fabric} from "fabric";
 
 export default function Page() {
     const canvasRef = useRef<any>(null);
     const {path} = useParams()
-    const song = newSong()
+    const [fabricComponents, setFabricComponents] = useState<{ canvas: null | fabric.Canvas }>({canvas: null})
     const width = window.innerWidth
+    useEffect(() => {
+        let canvas = null
+        if (canvasRef.current) {
+            canvas = new fabric.Canvas(canvasRef.current)
+            const circle = new fabric.Circle({radius: 100, left: width/ 4, top: + 150})
+            canvas.add(circle)
+            setFabricComponents({canvas: canvas})
+        }
+        return () => {
+            canvas?.dispose().then(canvasRef.current = null)
+        }
+    }, [])
+    const song = newSong()
 
-    let content = (<div>Loading...</div>)
-    if (path) {
-        content = (
-            <div>
-                <canvas ref={canvasRef} width={width} height={600} style={{border: "1px solid black"}}/>
-                <SongView startAudio={() => song.startAudio(`/api/audio/${path}.wav`)}
-                          transport={song.getTransport()}/>
-            </div>)
-    }
-    return content
+    return (<div>
+        <canvas ref={canvasRef} width={width} height={600} style={{border: "1px solid black"}}/>
+        <SongView startAudio={() => song.startAudio(`/api/audio/${path}.wav`)}
+                  transport={song.getTransport()}/>
+    </div>)
+
 }
